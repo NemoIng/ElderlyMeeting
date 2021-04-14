5164b44739d3ceffeede62a470e3c2854a361e30
NemoIng <n.ingendaa@gmail.com>
2021-04-14 15:07:41 +0200

    hello

--- app/src/main/java/com/example/elderlymeeting/MainActivity.java
+++ app/src/main/java/com/example/elderlymeeting/MainActivity.java
@@ -7,11 +7,13 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.example.elderlymeeting.ui.home.HomeFragment;
 import com.example.elderlymeeting.ui.login.LoginActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     Button signInBtn;
+    Button homeBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +26,14 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, LoginActivity.class);
                 startActivity(intent);
+            }
+        });
+
+        homeBtn = findViewById(R.id.home);
+        homeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                HomeFragment homeFragment = new HomeFragment();
             }
         });
     }
