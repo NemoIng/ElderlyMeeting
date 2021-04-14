f75188b308892dc995a8c3cfa6e6e61bc6c20905
NemoIng <n.ingendaa@gmail.com>
2021-04-14 14:10:31 +0200

    Initial commit

---
+++ app/src/main/java/com/example/elderlymeeting/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.elderlymeeting;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.elderlymeeting.ui.login.LoginActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button signInBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        signInBtn = findViewById(R.id.signIn);
+        signInBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
