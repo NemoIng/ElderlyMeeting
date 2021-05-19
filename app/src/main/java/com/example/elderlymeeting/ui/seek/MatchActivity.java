26828a9a1c9fe20a494ff444236eb6dfe63119b7
2maikel1 <2maikel1@gmail.com>
2021-05-19 14:52:51 +0200

    start match function

---
+++ app/src/main/java/com/example/elderlymeeting/ui/seek/MatchActivity.java
@@ -0,0 +1,16 @@
+package com.example.elderlymeeting.ui.seek;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.elderlymeeting.R;
+
+public class MatchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_match);
+    }
+}
--- app/src/main/java/com/example/elderlymeeting/ui/seek/SeekFragment.java
+++ app/src/main/java/com/example/elderlymeeting/ui/seek/SeekFragment.java
@@ -1,9 +1,12 @@
 package com.example.elderlymeeting.ui.seek;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -11,14 +14,35 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.elderlymeeting.HomeActivity;
 import com.example.elderlymeeting.R;
+import com.example.elderlymeeting.ui.registration.RegisterBio;
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.ArrayList;
 
 public class SeekFragment extends Fragment {
 
-    @Nullable
+    private Button matchBtn;
+    View view;
+
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_seek, container, false);
+        view = inflater.inflate(R.layout.fragment_seek, container, false);
+
+        matchBtn = (Button) view.findViewById(R.id.buttonMatch);
+        matchBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getActivity(), MatchActivity.class);
+                i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK );
+
+                startActivity(i);
+            }
+        });
+        return view;
     }
 }
